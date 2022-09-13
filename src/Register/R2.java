70f73c3a0922194088f88c64ed4902c131f66660
AYUSH BHARDWAJ <107998067+abhardwaj-aithent@users.noreply.github.com>
2022-09-13 17:05:25 +0530

    Update R2.java

--- src/Register/R2.java
+++ src/Register/R2.java
@@ -4,7 +4,10 @@ public class R2 {
 	public void m1 () {
 		System.out.println("changed now");
 		
-		System.out.Println("Changed by another developer ");
+		System.out.println("Changed by another developer ");
+		
+		System.out.println("Changed by 3rd  developer ");
+		
 	}
 	
 
